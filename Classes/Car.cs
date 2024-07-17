3508ffaa05200b3008f2dc2613d3f9812d07b034
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-07-17 09:19:54 -0500

    Exercises Complete

---
+++ Classes/Car.cs
@@ -0,0 +1,12 @@
+namespace Classes;
+
+public class Car
+{
+    // Create a Make property of type string that is public
+    // Create a Model property of type string that is public
+    // Create a Year property of type int that is public
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a new instance of the Car class
+            // Set values in the properties for the object
+            // Print the values of each property to the Console
+            var car = new Car();
+            car.Make = "Honda";
+            car.Model = "Accord";
+            car.Year = 2022;
+            
+            Console.WriteLine($"Make of car: {car.Make}");
+            Console.WriteLine($"Model of car: {car.Model}");
+            Console.WriteLine($"Year of car: {car.Year}");
+            
         }
     }
 }
